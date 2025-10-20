283ab92088d1d42c7c8878dc67198abdf5efc813
Eduu115 <serranotrenado.e@gmail.com>
2025-10-20 09:24:51 +0200

    start project

---
+++ src/main/java/apirest/productos/EduardoActividad1Application.java
@@ -0,0 +1,13 @@
+package apirest.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduardoActividad1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduardoActividad1Application.class, args);
+	}
+
+}
---
+++ src/test/java/apirest/productos/EduardoActividad1ApplicationTests.java
@@ -0,0 +1,13 @@
+package apirest.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduardoActividad1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
